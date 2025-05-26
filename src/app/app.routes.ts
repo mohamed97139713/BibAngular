eefe3b089f40e8531aca8badb586c75839596af9
kimokomo <karim.yassin@hotmail.com>
2025-05-26 11:25:03 +0100

    AbaoutUS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 import { BuecherverwaltungComponent } from './buecherverwaltung/buecherverwaltung.component';
 import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 export const routes: Routes = [
-     {
+    {
         path: '',
         component: HomeComponent
     },
     {
         path: 'buecherverwaltung',
         component: BuecherverwaltungComponent,
+    },
+    {
+        path: 'aboutus',
+        component: AboutusComponent,
     },
 ];
 
